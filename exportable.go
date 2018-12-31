34af33d7487efa97bef2b440910036c98fc985b3
Ganlv <ganlvtech@qq.com>
2019-01-01 03:13:01 +0800

    Initial commit

---
+++ exportable.go
@@ -0,0 +1,29 @@
+package cookiejar
+
+import (
+	"encoding/json"
+)
+
+type Entry = entry
+
+func (j *Jar) Entries() map[string]map[string]Entry {
+	return j.entries
+}
+
+func (j *Jar) SetEntries(entries map[string]map[string]Entry) {
+	j.entries = entries
+}
+
+func (j *Jar) JsonSerialize() ([]byte, error) {
+	return json.Marshal(j.Entries())
+}
+
+func (j *Jar) JsonDeserialize(data []byte) error {
+	entries := make(map[string]map[string]Entry, 0)
+	err := json.Unmarshal(data, &entries)
+	if err != nil {
+		return err
+	}
+	j.SetEntries(entries)
+	return nil
+}
